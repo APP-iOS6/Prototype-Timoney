1cb865354a6c4ed20fcd9e4f8a87c5865070a285
SeungWoo <kangsw1025@naver.com>
2024-09-24 15:37:08 +0900

    refactor : content view 수정

--- Timoney/Timoney/ContentView.swift
+++ Timoney/Timoney/ContentView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-//            Text("Hello, world!")
-//        }
-//        .padding()
-        MainView2()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
 
