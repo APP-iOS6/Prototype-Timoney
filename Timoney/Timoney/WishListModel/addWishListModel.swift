eb01fae791847f24591975a6261a46b7b88830f1
ParkJunYoung <j77777y@naver.com>
2024-09-25 17:13:12 +0900

    add

--- Timoney/Timoney/WishListModel/addWishListModel.swift
+++ Timoney/Timoney/WishListModel/addWishListModel.swift
@@ -13,4 +13,25 @@ class addWishList {
     var nameField: String = ""
     var valueItem: String = ""
     var imageURL: String = ""
+    
+    var formattedValueItem: String {
+        // valueItem이 비어있는지 확인
+        if valueItem.isEmpty {
+            return "" // 아무것도 입력되지 않으면 빈 문자열 반환
+        }
+        
+        // valueItem을 숫자로 변환
+        if let value = Int(valueItem) {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal // 3자리마다 쉼표 넣기
+            numberFormatter.locale = Locale(identifier: "ko_KR") // 한국식 통화 단위
+            if let formattedString = numberFormatter.string(from: NSNumber(value: value)) {
+                return "\(formattedString) 원" // 원화 표시
+            }
+        } else {
+            return "지원하지 않는 형식입니다" // 숫자가 아닌 경우
+        }
+        
+        return valueItem
+    }
 }
