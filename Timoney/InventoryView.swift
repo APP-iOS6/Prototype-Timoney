b34ca477c0628cd308aeb34efaf8ad84a6181d81
Soomin Kim <140694831+sumchive@users.noreply.github.com>
2024-09-26 14:02:27 +0900

    [chore] : 수정 버튼 액션 변경

--- Timoney/InventoryView.swift
+++ Timoney/InventoryView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct InventoryView: View {
     @State var isSheetShowing: Bool = false
+    @State var isEditShowing: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -21,57 +22,64 @@ struct InventoryView: View {
                                 .font(.system(size: 24))
                         }
                         
-                        NavigationLink(destination : EditView()) {
+                        Button(action: {
+                            isEditShowing = true
+                        }, label: {
                             Image(systemName: "pencil")
                                 .font(.system(size: 24))
                                 .padding(.trailing,20)
-                        }
+                        })
                     }
                     
                     Grid(horizontalSpacing: -15, verticalSpacing: 15) {
                         GridRow {
-                            ItemUIView2(icon: "3dicons", price: "20,000원", name: "미술용품", progress:"10")
-                            ItemUIView(icon: "bag", price: "521,000원", name: "가방", progress:"Group 8")
-                            ItemUIView(icon: "3dicons-2", price: "180,000원", name: "마이크", progress:"Group 7")
+                            ItemUIView2(icon: "3dicons", price: "20,000원", name: "미술용품", progress:"10", isEditShowing: $isEditShowing)
+                            ItemUIView(icon: "bag", price: "521,000원", name: "가방", progress:"Group 8",isEditShowing: $isEditShowing)
+                            ItemUIView(icon: "3dicons-2", price: "180,000원", name: "마이크", progress:"Group 7",isEditShowing: $isEditShowing)
                         }
                         
                         GridRow {
-                            ItemUIView(icon: "watch", price: "221,000원", name: "애플워치", progress:"Group 7")
-                            ItemUIView2(icon: "3dicons-4", price: "38,000원", name: "쇼핑", progress:"10")
-                            ItemUIView2(icon: "3dicons-5", price: "18,000원", name: "책", progress:"10")
+                            ItemUIView(icon: "watch", price: "221,000원", name: "애플워치", progress:"Group 7",isEditShowing: $isEditShowing)
+                            ItemUIView2(icon: "3dicons-4", price: "38,000원", name: "쇼핑", progress:"10", isEditShowing: $isEditShowing)
+                            ItemUIView2(icon: "3dicons-5", price: "18,000원", name: "책", progress:"10", isEditShowing: $isEditShowing)
                         }
                         
                         GridRow {
-                            ItemUIView2(icon: "3dicons-6", price: "4,500원", name: "커피", progress:"10")
-                            ItemUIView2(icon: "3dicons-7", price: "21,000원", name: "우산", progress:"10")
-                            ItemUIView(icon: "wallet", price: "490,000원", name: "메종마르지엘라 지갑",progress:"Group 8")
+                            ItemUIView2(icon: "3dicons-6", price: "4,500원", name: "커피", progress:"10", isEditShowing: $isEditShowing)
+                            ItemUIView2(icon: "3dicons-7", price: "21,000원", name: "우산", progress:"10", isEditShowing: $isEditShowing)
+                            ItemUIView(icon: "wallet", price: "490,000원", name: "메종마르지엘라 지갑",progress:"Group 8",isEditShowing: $isEditShowing)
                         }
                         
                         GridRow {
-                            ItemUIView2(icon: "3dicons", price: "20,000원", name: "미술용품", progress:"10")
+                            ItemUIView2(icon: "3dicons", price: "20,000원", name: "미술용품", progress:"10", isEditShowing: $isEditShowing)
                         }
                     }
                     .padding(.top, 20)
                     
-//                    // 수정하기 버튼
-//                    Button(action: {
-//                        isSheetShowing.toggle()
-//                    }, label: {
-//                        Text("수정하기")
-//                            .frame(width: 100, height: 40)
-//                            .background(Color(red: 3/255, green: 78/255, blue: 43/255))
-//                            .foregroundColor(.white)
-//                            .cornerRadius(8)
-//                            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 3)
-//                    })
-//                    .sheet(isPresented: $isSheetShowing, content: {
-//                        EditView()
-//                    })
-//                    .padding(.bottom, 20)
+                    
                 }
             }
             .navigationTitle("위시리스트")
             .navigationBarTitleDisplayMode(.automatic)
+            
+            // 수정하기 버튼
+            if isEditShowing {
+                Button(action: {
+                    isEditShowing = false
+                }, label: {
+                    Text("저장하기")
+                        .frame(width: 100, height: 40)
+                        .background(Color(red: 3/255, green: 78/255, blue: 43/255))
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 3)
+                })
+                .sheet(isPresented: $isSheetShowing, content: {
+                    EditView()
+                })
+                .padding(.bottom, 20)
+            }
+
         }
     }
 }
@@ -82,31 +90,57 @@ struct ItemUIView: View {
     var name: String
     var progress: String
     
+    @Binding var isEditShowing: Bool
+    
     var body: some View {
         VStack {
             // 아이콘
-            Image(icon)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 80, height: 80)
-                .padding()
-                .background(.white)
-                .cornerRadius(10)
-                .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 3)
+            ZStack {
+                Image(icon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 3)
+                
+                
+                if isEditShowing {
+                    HStack {
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "trash")
+                                .foregroundColor(Color.red)
+                                .padding(.leading, 80)
+                        }
+                        
+//                        //위치 바꾸기 버튼
+//                        Button(action: {
+//
+//                        }) {
+//                            Image(systemName: "arrow.up.arrow.down")
+//                                .foregroundColor(Color.black)
+//                        }
+                    }
+                    .padding(.top, 85)
+                }
+            }
             
             
             // 물건 이름
             Text(name)
                 .font(.caption2)
                 .fontWeight(.light)
-                //.foregroundColor(.black)
+            //.foregroundColor(.black)
                 .padding(.top, 10)
             
             // 물건 가격
             Text(price)
                 .font(.caption)
                 .fontWeight(.bold)
-                //.foregroundColor(.black)
+            //.foregroundColor(.black)
             
             // 게이지바
             Image(progress)
@@ -124,6 +158,8 @@ struct ItemUIView2: View {
     var name: String
     var progress: String
     
+    @Binding var isEditShowing: Bool
+    
     var body: some View {
         VStack {
             // 아이콘
@@ -138,15 +174,40 @@ struct ItemUIView2: View {
                     .shadow(color: .black.opacity(0.15), radius: 5, x: 0, y: 3)
                 
                 // 구매하기 버튼
-                Button(action: {
-                }) {
-                    Text("구매하기")
-                        .font(.caption)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .padding(5)
-                        .background(Color.gray)
-                        .cornerRadius(5)
+                
+                if !isEditShowing {
+                    Button(action: {
+                    }) {
+                        Text("구매하기")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(5)
+                            .background(Color.gray)
+                            .cornerRadius(5)
+                    }
+                }
+                
+                
+                if isEditShowing {
+                    HStack {
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "trash")
+                                .foregroundColor(Color.red)
+                                .padding(.leading, 80)
+                        }
+                        
+//                        //위치 바꾸기 버튼
+//                        Button(action: {
+//                            
+//                        }) {
+//                            Image(systemName: "arrow.up.arrow.down")
+//                                .foregroundColor(Color.black)
+//                        }
+                    }
+                    .padding(.top, 85)
                 }
             }
             
@@ -154,14 +215,14 @@ struct ItemUIView2: View {
             Text(name)
                 .font(.caption2)
                 .fontWeight(.light)
-                //.foregroundColor(.black)
+            //.foregroundColor(.black)
                 .padding(.top, 10)
             
             // 물건 가격
             Text(price)
                 .font(.caption)
                 .fontWeight(.bold)
-                //.foregroundColor(.black)
+            //.foregroundColor(.black)
             
             // 게이지바
             Image(progress)
